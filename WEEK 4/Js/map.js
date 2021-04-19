b8a2dda08f85915cf2a2f5b230f94a7b6336f5c5
jomh17 <73918238+jomh17@users.noreply.github.com>
2021-04-19 16:05:45 -0700

    .

--- WEEK 4/Js/map.js
+++ WEEK 4/Js/map.js
@@ -46,12 +46,21 @@ function readCSV(path){
 
 function mapCSV(data){
 	
+	// circle options
+	let circleOptions = {
+		radius: 5,
+		weight: 1,
+		color: 'white',
+		fillColor: 'dodgerblue',
+		fillOpacity: 1
+	}
+
 	// loop through each entry
 	data.data.forEach(function(item,index){
 		// create marker
-		let marker = L.marker([item.latitude,item.longitude])
+		let marker = L.circleMarker([item.latitude,item.longitude],circleOptions)
 
-		// add marker to featuregroup
+		// add marker to featuregroup		
 		markers.addLayer(marker)
 	})
 
@@ -61,4 +70,3 @@ function mapCSV(data){
 	// fit markers to map
 	map.fitBounds(markers.getBounds())
 }
-
