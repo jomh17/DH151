62ec12c182899d890b4b6040cb7007106847ed2b
jomh17 <73918238+jomh17@users.noreply.github.com>
2021-04-19 15:56:22 -0700

    .

--- WEEK 4/Js/map.js
+++ WEEK 4/Js/map.js
@@ -3,6 +3,10 @@ let map;
 let lat = 0;
 let lon = 0;
 let zl = 3;
+// path to csv data
+let path = "data/dunitz.csv";
+// global variables
+let markers = L.featureGroup();
 
 // initialize
 $( document ).ready(function() {
@@ -17,3 +21,44 @@ function createMap(lat,lon,zl){
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 }
+
+// function to read csv data
+function readCSV(path){
+	Papa.parse(path, {
+		header: true,
+		download: true,
+		complete: function(data) {
+			console.log(data);
+			
+			// map the data
+			mapCSV(data);
+
+		}
+	});
+}
+
+
+// initialize
+$( document ).ready(function() {
+	createMap(lat,lon,zl);
+	readCSV(path);
+});
+
+function mapCSV(data){
+	
+	// loop through each entry
+	data.data.forEach(function(item,index){
+		// create marker
+		let marker = L.marker([item.latitude,item.longitude])
+
+		// add marker to featuregroup
+		markers.addLayer(marker)
+	})
+
+	// add featuregroup to map
+	markers.addTo(map)
+
+	// fit markers to map
+	map.fitBounds(markers.getBounds())
+}
+
