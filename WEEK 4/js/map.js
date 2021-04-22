1845e165107bcc9b06c8cd12aa885c380a2e7342
jomh17 <73918238+jomh17@users.noreply.github.com>
2021-04-22 12:30:00 -0700

    .

--- WEEK 4/js/map.js
+++ WEEK 4/js/map.js
@@ -8,7 +8,24 @@ let path = "data/caribu.csv";
 // global variables
 let markers = L.featureGroup();
 
+var brownIcon = new L.Icon({
+	iconUrl: newFunction(),
+	shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+  });
+  
+  
 
+var map = L.map('map').setView([0,0], 2);
+
+
+function newFunction() {
+	return 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png';
+}
+L.marker([51.5, -0.09], {icon: brownIcon}).addTo(map);
 
 // create the map
 function createMap(lat,lon,zl){
