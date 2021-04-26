50cfe11fb11051d2e03446224c783be0952624eb
jomh17 <73918238+jomh17@users.noreply.github.com>
2021-04-26 15:23:26 -0700

    .

--- WEEK 5/js/map.js
+++ WEEK 5/js/map.js
@@ -3,8 +3,9 @@ let map;
 let lat = 0;
 let lon = 0;
 let zl = 3;
-let path = '';
+let path = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv';
 let markers = L.featureGroup();
+let csvdata;
 
 // initialize
 $( document ).ready(function() {
@@ -21,14 +22,20 @@ function createMap(lat,lon,zl){
 	}).addTo(map);
 }
 
+
 // function to read csv data
 function readCSV(path){
 	Papa.parse(path, {
 		header: true,
 		download: true,
-		complete: function(csvdata) {
-			console.log(csvdata);
-			
+		complete: function(data) {
+			console.log(data);
+			// put the data in a global variable
+			csvdata = data;
+
+			// call the mapCSV function to map the data
+			mapCSV();
 		}
 	});
 }
+
