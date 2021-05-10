b15ef85bbac720c8484139dbcd3a6f1d09a55388
jomh17 <73918238+jomh17@users.noreply.github.com>
2021-05-10 16:03:10 -0700

    .

--- WEEK 7/Js/map.js
+++ WEEK 7/Js/map.js
@@ -24,3 +24,12 @@ function createMap(lat,lon,zl){
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 }
+// function to map a geojson file
+function mapGeoJSON(){
+
+	// create the layer and add to map
+	geojson_layer = L.geoJson(geojson_data).addTo(map);
+
+	// fit to bounds
+	map.fitBounds(geojson_layer.getBounds())
+}
