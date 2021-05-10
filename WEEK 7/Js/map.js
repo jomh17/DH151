12341aa572a61531fb574cd90eff9b481bb2ea9d
jomh17 <73918238+jomh17@users.noreply.github.com>
2021-05-10 16:04:32 -0700

    .

--- WEEK 7/Js/map.js
+++ WEEK 7/Js/map.js
@@ -24,12 +24,38 @@ function createMap(lat,lon,zl){
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 }
-// function to map a geojson file
 function mapGeoJSON(){
 
 	// create the layer and add to map
-	geojson_layer = L.geoJson(geojson_data).addTo(map);
+	geojson_layer = L.geoJson(geojson_data, {
+		style: getStyle //call a function to style each feature
+	}).addTo(map);
 
 	// fit to bounds
 	map.fitBounds(geojson_layer.getBounds())
+}
+
+// style each feature
+function getStyle(feature){
+	return {
+		stroke: true,
+		color: 'white',
+		weight: 1,
+		fill: true,
+		fillColor: getColor(feature.properties['pop_est']),
+		fillOpacity: 0.8
+	}
+}
+
+// return the color for each feature
+function getColor(d) {
+
+	return d > 1000000000 ? '#800026' :
+		   d > 500000000  ? '#BD0026' :
+		   d > 200000000  ? '#E31A1C' :
+		   d > 100000000  ? '#FC4E2A' :
+		   d > 50000000   ? '#FD8D3C' :
+		   d > 20000000   ? '#FEB24C' :
+		   d > 10000000   ? '#FED976' :
+					  '#FFEDA0';
 }
